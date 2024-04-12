1383fe38680173547a2c1782b0fafbf87b857901
[Omedobi] <ikennaanyawuike@gmail.com>
2024-04-12 03:40:21 +0100

    Add Extract_text.py for PDF text extraction

--- Text Extraction from PDF/Extract_text.py
+++ Text Extraction from PDF/Extract_text.py
@@ -3,7 +3,9 @@ import PyPDF2
 
 pdf = open("Text Extraction from PDF\data\EORTC404112-QLQ-HCC18-English.pdf", "rb")
 reader = PyPDF2.PdfReader(pdf)
-page = reader.pages[0]
-text = page.extract_text()
-print(text)
+
+for page_no, page in enumerate(reader.pages):
+    text = page.extract_text()
+    print(f"Page {page_no + 1}:\n{text}\n")
+    
 pdf.close()
