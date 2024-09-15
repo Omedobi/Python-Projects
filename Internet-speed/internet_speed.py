4ffa2b5d1720a725ecccacbda3d1a7fb00def45f
Anyawuike Ikenna Omedobi <100056864+Omedobi@users.noreply.github.com>
2024-09-15 08:11:13 +0100

    Update internet_speed.py

--- Internet-speed/internet_speed.py
+++ Internet-speed/internet_speed.py
@@ -1,1 +1,19 @@
+#pip install speedtest
 
+import speedtest as st
+
+def Speed_test():
+  test = st.Speed_test()
+
+  down_Speed = test.download()
+  down_speed = round(down_speed / 10**6, 2)
+  print("Download speed in Mbps: ", down_speed)
+
+  up_speed = test.upload()
+  up_speed = round(up_speed / 10**6, 2)
+  print("Upload speed in Mbps: ", up_speed)
+
+  ping = test.results.ping
+  print("Ping: ", ping)
+
+speed_test()
